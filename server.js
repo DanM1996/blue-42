0039d05a6963b5d26b4e9d384d7b32c14d6d739c
EddieH <ehajek@outlook.com>
2021-08-26 20:11:57 -0400

    2nd API (GAMES) working

--- server.js
+++ server.js
@@ -21,35 +21,47 @@ const defaultClient = cfb.ApiClient.instance;
 const ApiKeyAuth = defaultClient.authentications['ApiKeyAuth'];
 ApiKeyAuth.apiKey = "Bearer " + cfbdKey;
 
-//API Call
+//---API Calls---
 
-// - FBS team list
-var apiInstance = new cfb.TeamsApi();
-var opts = { 
-  'year': 2021 // Number | Year filter
-};
+//--FBS team list--
+function fbsList() {
+    let apiInstance = new cfb.TeamsApi();
+    let opts = { 
+    'year': 2021 // Number | Year filter
+    };
 
-apiInstance.getFbsTeams(opts)
-.then(function(data){
-//     return data.json();})
- //   .then(function(response) {
-    console.log('API called successfully. Returned data: ' + data[0].id);
-    //fs.writeFileSync("./data.json", JSON.stringify(data[0], null, 2));
-}, function(error) {
-    console.error(error);
-})
+    apiInstance.getFbsTeams(opts)
+    .then(function(data){
+        console.log('API called successfully. Returned data: ' + data[0].id);
+        //fs.writeFileSync("./data.json", JSON.stringify(data[0], null, 2));
+    }, function(error) {
+        console.error(error);
+    })
+}
 
+fbsList(); 
 
+//-- Games list--
+function games() {
+    let apiInstance = new cfb.GamesApi();
+    var year = 20; // Number | Year/season filter for games
 
+    var opts = { 
+      'week': 56, // Number | Week filter
+      'seasonType': "regular", // String | Season type filter (regular or postseason)
+      'team': "team_example", // String | Team
+      'home': "home_example", // String | Home team filter
+      'away': "away_example", // String | Away team filter
+      'conference': "conference_example", // String | Conference abbreviation filter
+      'id': 56 // Number | id filter for querying a single game
+    };
+    apiInstance.getGames(year, opts)
+    .then(function(data){
+        console.log('API called successfully. Returned data: ' + data[0].id);
+        //fs.writeFileSync("./ExampleData/Game_data.json", JSON.stringify(data[0], null, 2));
+    }, function(error) {
+        console.error(error);
+    })
+}
 
-
-
-
-// ​/teams​/fbs
-// - FBS team list
-
-// ​/teams​/fbs
-// - FBS team list
-
-
-
+games(); 
