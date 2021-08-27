60f40a6f45d158567072832ddc9b4023f0974277
Dan <bmdmartinez@gmail.com>
2021-08-26 20:34:43 -0400

    started handlebars and css, tested out some styles and things

---
+++ server.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+    res.render('test');
+})
+app.listen(PORT, () => console.log('Now listening'));
